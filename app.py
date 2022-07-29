22e0c729d3b57b748a5954b0f4dba1770357b73c
AKINTAYO AKINPELU <AKINTAYO.AKINPELU@TRANTERIT.LOCAL>
2022-07-29 12:19:13 +0100

    Template Update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import sys
 import project_name
 from project_name.logger import logging
-from project_name.exception import nameException
+from project_name.exception import Project_nameException
 
 app=Flask(__name__)
 
@@ -11,7 +11,7 @@ def index():
     try:
         raise Exception("We are testing custom exception")
     except Exception as e:
-        project_name = nameException(e, sys)
+        project_name = Project_nameException(e, sys)
         logging.info(project_name.error_message)
         logging.info("We are testing logging module")
     return "CI CD pipeline has been established."
